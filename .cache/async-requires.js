a7ed517e9e0979710b4320385d6b67338766c5a1
gcmillar <gcmillar@ncsu.edu>
2020-08-06 12:15:58 -0400

    cleaning node & mobile display fix attempt

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,18 +2,16 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---src-pages-404-js": () => import("/Users/garrettmillar/gcmillar.github.io/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-crowd-scores-js": () => import("/Users/garrettmillar/gcmillar.github.io/src/pages/CrowdScores.js" /* webpackChunkName: "component---src-pages-crowd-scores-js" */),
-  "component---src-pages-gradient-js": () => import("/Users/garrettmillar/gcmillar.github.io/src/pages/Gradient.js" /* webpackChunkName: "component---src-pages-gradient-js" */),
-  "component---src-pages-index-js": () => import("/Users/garrettmillar/gcmillar.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-numilli-js": () => import("/Users/garrettmillar/gcmillar.github.io/src/pages/Numilli.js" /* webpackChunkName: "component---src-pages-numilli-js" */),
-  "component---src-pages-page-2-js": () => import("/Users/garrettmillar/gcmillar.github.io/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
-  "component---src-pages-quidco-js": () => import("/Users/garrettmillar/gcmillar.github.io/src/pages/Quidco.js" /* webpackChunkName: "component---src-pages-quidco-js" */),
-  "component---src-pages-suggestv-js": () => import("/Users/garrettmillar/gcmillar.github.io/src/pages/Suggestv.js" /* webpackChunkName: "component---src-pages-suggestv-js" */),
-  "component---src-pages-thanks-js": () => import("/Users/garrettmillar/gcmillar.github.io/src/pages/Thanks.js" /* webpackChunkName: "component---src-pages-thanks-js" */),
-  "component---src-pages-vivy-js": () => import("/Users/garrettmillar/gcmillar.github.io/src/pages/Vivy.js" /* webpackChunkName: "component---src-pages-vivy-js" */),
-  "component---src-pages-yunojuno-js": () => import("/Users/garrettmillar/gcmillar.github.io/src/pages/Yunojuno.js" /* webpackChunkName: "component---src-pages-yunojuno-js" */)
+  "component---src-pages-404-js": () => import("./../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-crowd-scores-js": () => import("./../../src/pages/CrowdScores.js" /* webpackChunkName: "component---src-pages-crowd-scores-js" */),
+  "component---src-pages-gradient-js": () => import("./../../src/pages/Gradient.js" /* webpackChunkName: "component---src-pages-gradient-js" */),
+  "component---src-pages-index-js": () => import("./../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-numilli-js": () => import("./../../src/pages/Numilli.js" /* webpackChunkName: "component---src-pages-numilli-js" */),
+  "component---src-pages-page-2-js": () => import("./../../src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
+  "component---src-pages-quidco-js": () => import("./../../src/pages/Quidco.js" /* webpackChunkName: "component---src-pages-quidco-js" */),
+  "component---src-pages-suggestv-js": () => import("./../../src/pages/Suggestv.js" /* webpackChunkName: "component---src-pages-suggestv-js" */),
+  "component---src-pages-thanks-js": () => import("./../../src/pages/Thanks.js" /* webpackChunkName: "component---src-pages-thanks-js" */),
+  "component---src-pages-vivy-js": () => import("./../../src/pages/Vivy.js" /* webpackChunkName: "component---src-pages-vivy-js" */),
+  "component---src-pages-yunojuno-js": () => import("./../../src/pages/Yunojuno.js" /* webpackChunkName: "component---src-pages-yunojuno-js" */)
 }
-
-exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/garrettmillar/gcmillar.github.io/.cache/data.json")
 
